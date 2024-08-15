3b68df69449905093737cb36c0421ced1f91c5d0
Legion204 <ri46801@gmail.com>
2024-08-15 13:37:30 +0600

    added login and register form

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,48 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
+
+    const handelLogin = e =>{
+        e.preventDefault()
+        const email = e.target.email.value
+        const password = e.target.password.value
+        console.log(email,password);
+    }
+
     return (
-        <div>
-            login
-        </div>
+        <>
+            <div className="hero bg-base-200 min-h-screen">
+                <div className="hero-content flex-col lg:flex-row lg:gap-16">
+                    <div className="text-center lg:text-left">
+                    <h1 className="text-5xl font-bold">Login To Your <span className='text-[#8338ec]'>Aura Shop</span> Account</h1>
+                        <p className="py-6">
+                            We're glad to see you again. Please enter your details to continue shopping.
+                        </p>
+                        <p>Don't have an account? <Link to={'/register'} className='text-blue-500 underline'>Register</Link> to create a new one.</p>
+                    </div>
+                    <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+                        <form onSubmit={handelLogin} className="card-body">
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="email" name='email' placeholder="email" className="input input-bordered" required />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input type="password" placeholder="password" name='password' className="input input-bordered" required />
+                            </div>
+                            <div className="form-control mt-6">
+                                <button type='submit' className="btn text-white bg-[#8338ec]">Login</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </>
     );
 };
 
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,10 +1,56 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
+
+    const handelRegister = e =>{
+        e.preventDefault()
+        const name = e.target.name.value
+        const email = e.target.email.value
+        const password = e.target.password.value
+        console.log(name,email,password);
+    }
+
     return (
-        <div>
-            Register
-        </div>
+        <>
+            <div className="hero bg-base-200 min-h-screen">
+                <div className="hero-content flex-col lg:flex-row lg:gap-16">
+                    <div className="text-center lg:text-left">
+                        <h1 className="text-5xl font-bold">Create Your <span className='text-[#8338ec]'>Aura Shop</span> Account</h1>
+                        <h1 className="text-3xl font-bold pt-6">Join our community and start shopping today!</h1>
+                        <p className="py-6">
+                        Fill in the details to create your account and enjoy exclusive offers, personalized recommendations, and more.
+                        </p>
+                        <p>Already have an account? <Link to={'/'} className='text-blue-500 underline'>Login</Link> to continue shopping.</p>
+                    </div>
+                    <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+                        <form onSubmit={handelRegister} className="card-body">
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Name</span>
+                                </label>
+                                <input type="text" name='name' placeholder="Name" className="input input-bordered" required />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="email" name='email' placeholder="Email" className="input input-bordered" required />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input type="password" placeholder="Password" name='password' className="input input-bordered" required />
+                            </div>
+                            <div className="form-control mt-6">
+                                <button type='submit' className="btn text-white bg-[#8338ec]">Login</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </>
     );
 };
 
